41fc5c6c79260f2fcf2f4564b7dbc50cf704f8f6
nazmul-hasan <nazmul.tex99@gmail.com>
2023-09-18 20:29:58 +0600

    Live test

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Second live test",
-      theme: ThemeData.dark( useMaterial3: true),
+      theme: ThemeData.light( useMaterial3: true),
       home: const HomePage(),
     );
   }
@@ -23,8 +24,45 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar( title: const Text("My shopping List"),
+      centerTitle: true,
+      elevation: 8,
+      backgroundColor: Colors.purple,),
+      body: const SingleChildScrollView(
+        padding: EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ListTile(
+            leading: Icon(Icons.shopping_basket,),
+            title: Text("Apple",
+            style: TextStyle(fontSize: 16),),
+            ),
+            ListTile(
+            leading: Icon(Icons.shopping_basket,),
+            title: Text("Banan",
+            style: TextStyle(fontSize: 16),),
+            ),
+            ListTile(
+            leading: Icon(Icons.shopping_basket,),
+            title: Text("Milk",
+            style: TextStyle(fontSize: 16),),
+            ),
+            ListTile(
+            leading: Icon(Icons.shopping_basket,),
+            title: Text("Eggs",
+            style: TextStyle(fontSize: 16),),
+            ),
+            ListTile(
+            leading: Icon(Icons.shopping_basket,),
+            title: Text("Orange",
+            style: TextStyle(fontSize: 16),),
+            ),
+          ],
+        ),
+      ),
     );
 
   }
 }
+//https://github.com/nazmul-hasan-24/live_test_6
